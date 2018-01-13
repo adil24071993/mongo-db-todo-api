0af8e005f036d239eaf58e72a1c3cbb24840e2bc
Syed Adil <a.syedadil@gmail.com>
2018-01-13 13:06:56 +0530

    Added POST todo route and refactoring

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -10,28 +10,28 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   }
   console.log('Connected to MongoDB server');
 
-  // db.collection('Todos').insertOne({
-  //   test: 'Something to do',
-  //   completed: false
-  // }, (err, result) => {
-  //   if(err){
-  //     return console.log('Unable to insert todo', err);
-  //   }
-  //   console.log(JSON.stringify(result.ops, undefined, 2))
-  // });
-
-  db.collection('Users').insertOne({
-    name: 'Adil',
-    age: '24',
-    location: 'India'
-  }, (err, result)=>{
+  db.collection('Todos').insertOne({
+    test: 'Something to do',
+    completed: false
+  }, (err, result) => {
     if(err){
-      return console.log('Unable to connect to Users', err)
+      return console.log('Unable to insert todo', err);
     }
-
-    console.log(JSON.stringify(result.ops, undefined, 2));
-    console.log(result.ops[0]._id.getTimestamp());
+    console.log(JSON.stringify(result.ops, undefined, 2))
   });
+
+  // db.collection('Users').insertOne({
+  //   name: 'Adil',
+  //   age: '24',
+  //   location: 'India'
+  // }, (err, result)=>{
+  //   if(err){
+  //     return console.log('Unable to connect to Users', err)
+  //   }
+  //
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  //   console.log(result.ops[0]._id.getTimestamp());
+  // });
 
 
 
