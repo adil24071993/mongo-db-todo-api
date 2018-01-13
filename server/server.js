c7bdb2a82171125e78dc7762cd44b8831cc5d00b
Syed Adil <a.syedadil@gmail.com>
2018-01-13 15:06:23 +0530

    Added DELETE todo api

--- server/server.js
+++ server/server.js
@@ -50,6 +50,28 @@ app.get('/todos/:id', (req, res)=>{
   });
 });
 
+app.delete('/todos/:id',(req, res)=>{
+  var id = req.params.id;
+  if(!ObjectID.isValid(id)){
+    return res.status(404).send({
+      "errorMessage" : "ID Not Valid"
+    });
+  }
+  Todo.findByIdAndRemove(id).then((todo)=>{
+    if(!todo){
+      return res.status(404).send({
+        "errorMessage" : "ID Not found"
+      });
+    }else{
+      res.send({todo});
+    }
+  },(e)=>{
+    res.status(400).send({
+      "errorMessage" : "Not a valid ID"
+    });
+  });
+});
+
 app.listen(port, ()=>{
   console.log('Server started at port ', port);
 });
